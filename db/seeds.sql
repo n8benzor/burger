e33cd3daf7e13cad528a7317e32e9057ced1b62f
Nathan <n8benzor@gmail.com>
2019-08-19 17:17:05 -0700

    completed mvc

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Mushroom and Swiss');
